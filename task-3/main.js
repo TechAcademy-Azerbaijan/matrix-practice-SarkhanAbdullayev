04fe5899b8d7cb06345e843bbff37473c95e6a0d
Sarkhan <syako44@gmail.com>
2022-07-06 22:49:32 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -1,10 +1,24 @@
-// Use this matrix for solution
-
 let matrix = [
     [2, 5, 3, 1, 4],
     [3, 5, 2, 3, 4],
-    [4, 6, 1, 2, 3],
-    [4, 5, 6, 7, 7]
+    [4, 6, 1, 1, 3],
+    [4, 5, 6, 1, 7]
 ]
 
-// Write code here. 
+let indexes = []
+
+let min = Infinity
+
+for (let i = 0 ; i < matrix.length ; i++ ) {
+        for ( let j = 0 ; j < matrix[i].length ; j++ ) {
+          if( matrix[i][j] < min ){
+            min = matrix[i][j]
+            indexes = [];
+            indexes.push(i)
+          }
+          else if (matrix[i][j] == min && !indexes.includes(i)) {
+            indexes.push(i);
+          }
+    }
+  }
+console.log(indexes);
