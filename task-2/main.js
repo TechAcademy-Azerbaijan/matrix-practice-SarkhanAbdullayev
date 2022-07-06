337d074f1d80e2da83d5b2ed08fe84641829f2a4
Sarkhan <syako44@gmail.com>
2022-07-06 22:48:19 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -1,13 +1,21 @@
-// Use this matrix for solution
-
 let matrix = [
     [1, 2, 3, 4],
     [5, 6, 7, 8],
     [9, 1, 2, 3],
     [4, 5, 6, 7]
-];
+]
+let r = 3
+let c = 2
 
-let r = 3;
-let c = 2;
+let newmatrix = []
+let arr = []
 
-// Write code here. 
+for (let i = 0 ; i < r ; i++ ) {
+    for ( let j = 0 ; j < c ; j++ ) {
+        arr.push(matrix[i][j])
+        
+    }
+    newmatrix.push(arr)
+    arr = []
+    
+}console.log(newmatrix);
